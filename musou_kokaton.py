98fff635e34c78f4d339607c472235d93dc1da98
c0a2422947 <c0a2422947@edu.teu.ac.jp>
2025-12-09 16:59:28 +0900

    追加機能6: 弾幕

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -141,14 +141,14 @@ class Beam(pg.sprite.Sprite):
     """
     ビームに関するクラス
     """
-    def __init__(self, bird: Bird):
+    def __init__(self, bird: Bird, angle0: int=0):
         """
         ビーム画像Surfaceを生成する
         引数 bird：ビームを放つこうかとん
         """
         super().__init__()
         self.vx, self.vy = bird.dire
-        angle = math.degrees(math.atan2(-self.vy, self.vx))
+        angle = math.degrees(math.atan2(-self.vy, self.vx)) + angle0
         self.image = pg.transform.rotozoom(pg.image.load(f"fig/beam.png"), angle, 1.0)
         self.vx = math.cos(math.radians(angle))
         self.vy = -math.sin(math.radians(angle))
@@ -241,6 +241,19 @@ class Score:
         self.image = self.font.render(f"Score: {self.value}", 0, self.color)
         screen.blit(self.image, self.rect)
 
+class NeoBeam():
+    """
+    NeoBeam の Docstring
+    """
+    def __init__(self, bird: Bird, num: int):
+        self.bird = bird
+        self.num = num
+
+    def gen_beams(self) -> list[Beam]:
+        beams = []
+        for angle in range(-50, 51, int(125/self.num)):
+            beams.append(Beam(self.bird, angle))
+        return beams
 
 def main():
     pg.display.set_caption("真！こうかとん無双")
@@ -263,10 +276,9 @@ def main():
                 return 0
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 beams.add(Beam(bird))
-            if event.type == pg.KEYDOWN and event.key == pg.K_LSHIFT:
-                bird.speed = 20
-            else:
-                bird.speed = 10
+            if event.type == pg.KEYDOWN and event.key == pg.K_z:
+                neo_beam = NeoBeam(bird, 5)
+                beams.add(neo_beam.gen_beams())
         screen.blit(bg_img, [0, 0])
 
         if tmr%200 == 0:  # 200フレームに1回，敵機を出現させる
